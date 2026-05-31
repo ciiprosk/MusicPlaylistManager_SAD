cc6cf666de0158745f00689b7f21562f78fe1c5c
ciiprosk <rosapassaro28@gmail.com>
2026-05-31 23:47:39 +0200

    aggiunto class diagram e nuoco component per playlistcard

---
+++ src/main/java/it/diem/unisa/musicmanager/controller/PlaylistCardController.java
@@ -0,0 +1,11 @@
+package it.diem.unisa.musicmanager.controller;
+
+import javafx.event.ActionEvent;
+
+public class PlaylistCardController {
+    public void handlePlay(ActionEvent actionEvent) {
+    }
+
+    public void handleMenu(ActionEvent actionEvent) {
+    }
+}
