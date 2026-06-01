971bdd6283e83ed96515a69aab3901c192ba2a0e
OmarSilano <silanoomar@gmail.com>
2026-06-01 17:19:18 +0200

    TrackDAO

--- src/main/java/it/diem/unisa/musicmanager/dao/TrackDAO.java
+++ src/main/java/it/diem/unisa/musicmanager/dao/TrackDAO.java
@@ -1,13 +1,46 @@
 package it.diem.unisa.musicmanager.dao;
 
+import it.diem.unisa.musicmanager.exception.FilePathException;
 import it.diem.unisa.musicmanager.model.Track;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public class TrackDAO implements DAO<Track> {
 
+    private final String filePath;
+
+    public TrackDAO(String filePath) {
+
+        this.filePath = filePath;
+
+        createFilePath();
+
+    }
+
+    private void createFilePath() {
+
+        File file = new File(filePath);
+
+        if (!file.exists()) { //se il file non esiste ancora, lo creo
+
+            try {
+                file.createNewFile();   //provo a creare il file
+
+                Files.write(Paths.get(filePath), "[]".getBytes());
+
+            } catch (IOException e) {
+                throw new FilePathException("Error: File Path not created!");
+            }
+
+        }
+
+    }
 
     /**
      * @return
---
+++ src/main/java/it/diem/unisa/musicmanager/exception/FilePathException.java
@@ -0,0 +1,7 @@
+package it.diem.unisa.musicmanager.exception;
+
+public class FilePathException extends RuntimeException {
+    public FilePathException(String message) {
+        super(message);
+    }
+}
