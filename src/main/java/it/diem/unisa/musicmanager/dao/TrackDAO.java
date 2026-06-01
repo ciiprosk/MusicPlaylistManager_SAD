37799be34a612e3765e9237b3970d464fcafe974
OmarSilano <silanoomar@gmail.com>
2026-06-01 16:53:23 +0200

    DAO interface

---
+++ src/main/java/it/diem/unisa/musicmanager/dao/TrackDAO.java
@@ -0,0 +1,54 @@
+package it.diem.unisa.musicmanager.dao;
+
+import it.diem.unisa.musicmanager.model.Track;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class TrackDAO implements DAO<Track> {
+
+
+    /**
+     * @return
+     */
+    @Override
+    public List<Track> selectAll() {
+
+
+
+    }
+
+    /**
+     * @param track
+     */
+    @Override
+    public void insert(Track track) {
+
+    }
+
+    /**
+     * @param track
+     */
+    @Override
+    public void update(Track track) {
+
+    }
+
+    /**
+     * @param track
+     */
+    @Override
+    public void delete(Track track) {
+
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public Optional<Track> searchById(UUID id) {
+        return Optional.empty();
+    }
+}
