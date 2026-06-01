7480853895604f6a9ad98e57a17761f3b9599e37
simon <simoneluongo02@gmail.com>
2026-06-01 16:50:49 +0200

    Popup di Aggiunta Brano aggiunto

--- src/main/java/it/diem/unisa/musicmanager/controller/TracksController.java
+++ src/main/java/it/diem/unisa/musicmanager/controller/TracksController.java
@@ -1,8 +1,31 @@
 package it.diem.unisa.musicmanager.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import javafx.event.ActionEvent;
 
 public class TracksController {
     public void openAddTrackModal(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/it/diem/unisa/musicmanager/pages/addSong.fxml")
+            );
+
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Add Track");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root));
+
+            stage.showAndWait();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
