33642e2196eae3aad12be9cda6d855a89820adc1
ciiprosk <rosapassaro28@gmail.com>
2026-06-01 15:17:12 +0200

    inizio della creazione della classe playlost

--- src/main/java/it/diem/unisa/musicmanager/model/Playlist.java
+++ src/main/java/it/diem/unisa/musicmanager/model/Playlist.java
@@ -1,4 +1,28 @@
 package it.diem.unisa.musicmanager.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Playlist {
+    private String name;
+    private final UUID id = UUID.randomUUID();
+    private List<UUID> tracks;
+
+    public Playlist(String name) {
+        this.name = name;
+        this.tracks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public UUID getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
