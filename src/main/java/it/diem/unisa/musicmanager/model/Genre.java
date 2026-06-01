ed0604c4ab8b826db80e099e3c40a2849f46f535
OmarSilano <silanoomar@gmail.com>
2026-06-01 15:16:12 +0200

    track e genere

---
+++ src/main/java/it/diem/unisa/musicmanager/model/Genre.java
@@ -0,0 +1,18 @@
+package it.diem.unisa.musicmanager.model;
+
+public enum Genre {
+
+    POP,
+    ROCK,
+    HIPHOP,
+    EDM,
+    JAZZ,
+    CLASSICAL,
+    REGGAE,
+    DISCO,
+    METAL,
+    FUNK,
+    INDIE,
+    COUNTRY
+
+}
