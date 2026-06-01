68694be98bbf5feb5430a42462f6cbf5ea62bd99
ciiprosk <rosapassaro28@gmail.com>
2026-06-01 17:52:12 +0200

    creato metodo createFileJSON

--- src/main/java/it/diem/unisa/musicmanager/dao/JSONAbstractDAO.java
+++ src/main/java/it/diem/unisa/musicmanager/dao/JSONAbstractDAO.java
@@ -1,4 +1,25 @@
 package it.diem.unisa.musicmanager.dao;
 
+import it.diem.unisa.musicmanager.exception.FilePathException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public abstract class JSONAbstractDAO {
+        protected void createFileJSON(String filePath) {
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                    // Scrive l'array vuoto per Gson
+                    Files.write(Paths.get(filePath), "[]".getBytes());
+
+                } catch (IOException e) {
+                    throw new FilePathException("Error: File Path not created!");
+                }
+            }
+        }
 }
