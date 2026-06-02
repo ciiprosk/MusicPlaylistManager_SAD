252b6bb42e57ff690431ab7461f3c444021dbc35
OmarSilano <silanoomar@gmail.com>
2026-06-03 00:46:07 +0200

    update di TrackService

--- src/main/java/it/diem/unisa/musicmanager/service/TrackService.java
+++ src/main/java/it/diem/unisa/musicmanager/service/TrackService.java
@@ -24,7 +24,9 @@ public class TrackService {
     }
 
 
-    public Optional<Track> getTrackById(UUID id){ return null;}
+    public Optional<Track> getTrackById(UUID id){
+        return null;
+    }
 
     public Optional<String> addTrack(String title, String author, Genre genre, String songPath, int songLength, String year){
         try{
@@ -44,11 +46,60 @@ public class TrackService {
         }
     }
 
-    public Optional<String> updateTrack(Track track){ return null;}
-    public Optional<String> deleteTrack(Track track){ return null;}
+    public Optional<String> updateTrack(UUID trackId, String newTitle, String newAuthor, Genre newGenre, String newYear){
+        //come parametri del metodo, tutte le info che possono cambiare di una traccia + Id perché ci serve per cercare nel DAO
+        Optional<Track> optionalTrack = trackDAO.searchById(trackId);
 
-    private void updateInitState(Track track){}
+        if (optionalTrack.isEmpty()) {
+            return Optional.of("Error: Track not found.");
+        }
 
-    public Optional<String> verify(Track track){ return null;}
+        Track track = optionalTrack.get();  //se non è vuoto, non è più Optional
+
+        try {
+
+            //creo una traccia temporanea
+            //così avviene anche validazione dei nuovi campi
+            Track tempTrack = new Track(newTitle, newAuthor, newGenre, track.getSongPath(), track.getSongLength(), newYear);
+
+            //verifica duplicato: id diversi, stesso titolo e stesso autore
+            boolean isDuplicate = sharedState.getALlTracks().stream()
+                    .anyMatch(t -> !t.getId().equals(trackId) && t.isDuplicate(tempTrack));
+
+            if (isDuplicate)
+                return Optional.of("Error: A track with this title and author already exists!");
+
+            //aggiornamento campi dell'oggetto track
+
+            track.setTitle(newTitle);
+            track.setAuthor(newAuthor);
+            track.setGenre(newGenre);
+            track.setYear(newYear);
+
+            //update della traccia con DAO
+            trackDAO.update(track);
+
+            //avviso interfaccia grafica del cambiamento
+            updateInState(track);
+
+            return Optional.empty();    //vuol dire che non c'è nessun errore
+
+        } catch (TrackInfoException e) {    //finisco qui se non va a buon fine la validazione
+            return Optional.of(e.getMessage());
+        }
+
+    }
+
+    public Optional<String> deleteTrack(Track track){
+        return null;
+    }
+
+    private void updateInState(Track track){
+
+    }
+
+    public Optional<String> verify(Track track){
+        return null;
+    }
 
 }
