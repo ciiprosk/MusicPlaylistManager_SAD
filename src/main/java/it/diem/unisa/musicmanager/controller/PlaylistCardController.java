7615d41b3e4cdf396787f039fcc4e3fc5d9ea3ee
simon <simoneluongo02@gmail.com>
2026-06-02 21:49:26 +0200

    Nuove Schermate

--- src/main/java/it/diem/unisa/musicmanager/controller/PlaylistCardController.java
+++ src/main/java/it/diem/unisa/musicmanager/controller/PlaylistCardController.java
@@ -1,11 +1,87 @@
 package it.diem.unisa.musicmanager.controller;
 
-import javafx.event.ActionEvent;
+import it.diem.unisa.musicmanager.model.Playlist;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+/**
+ * Controller di una card playlist (playlistCard.fxml).
+ * Mostra nome e numero di tracce, e offre le azioni Play / Modifica / Menu.
+ * La playlist viene passata tramite {@link #setPlaylist(Playlist)} prima
+ * di mostrare la card.
+ */
 public class PlaylistCardController {
-    public void handlePlay(ActionEvent actionEvent) {
+
+    // --- Campi dell'interfaccia, collegati agli fx:id in playlistCard.fxml ---
+    @FXML private Label lblPlaylistIcon;
+    @FXML private Label lblNome;
+    @FXML private Label lblTracce;
+    @FXML private Button btnPlay;
+    @FXML private Button btnModify;
+    @FXML private Button btnMenu;
+
+    // La playlist mostrata da questa card.
+    private Playlist playlist;
+
+    /**
+     * Imposta la playlist della card e riempie le etichette.
+     *
+     * @param playlist la playlist da mostrare
+     */
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+        lblNome.setText(playlist.getName());
+        int n = playlist.getTracks().size();
+        lblTracce.setText(n + (n == 1 ? " traccia" : " tracce"));
     }
 
-    public void handleMenu(ActionEvent actionEvent) {
+    /**
+     * Click su Play: avvia la riproduzione della playlist.
+     * (Da collegare al PlayerService quando disponibile.)
+     */
+    @FXML
+    private void handlePlay() {
+        // TODO: riproduzione della playlist
+    }
+
+    /**
+     * Click su Modifica: apre la finestra di modifica della playlist (editPlaylist).
+     */
+    @FXML
+    private void handleModify() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/it/diem/unisa/musicmanager/pages/editPlaylist.fxml"));
+            Parent root = loader.load();
+
+            // Passiamo la playlist al controller della modifica.
+            EditPlaylistController controller = loader.getController();
+            //controller.setPlaylist(playlist);
+            // Quando il PlaylistService sara' attivo, qui passeremo anche il service:
+            // controller.setPlaylistService(playlistService);
+
+            Stage stage = new Stage();
+            stage.setTitle("Modifica Playlist");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Click su Menu: apre le altre azioni della playlist.
+     */
+    @FXML
+    private void handleMenu() {
+        // TODO: menu con altre azioni (es. elimina, dettagli)
     }
 }
--- src/main/java/it/diem/unisa/musicmanager/controller/PlaylistController.java
+++ src/main/java/it/diem/unisa/musicmanager/controller/PlaylistController.java
@@ -1,4 +1,48 @@
 package it.diem.unisa.musicmanager.controller;
 
+import it.diem.unisa.musicmanager.model.Playlist;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * Controller della schermata "Playlists".
+ * Mostra le playlist come griglia di card e permette di crearne di nuove
+ * aprendo la finestra "Crea Playlist".
+ */
 public class PlaylistController {
+
+    // --- Campi dell'interfaccia, collegati agli fx:id in playlist.fxml ---
+    @FXML private TextField searchBar;
+    @FXML private FlowPane playlistsGrid;
+
+    /**
+     * Apre la finestra modale "Crea Playlist".
+     * Per ora apre solo la finestra (senza service).
+     *
+     * @param actionEvent evento generato dal click sul pulsante "Add Playlist"
+     */
+    @FXML
+    private void handleAdd(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/it/diem/unisa/musicmanager/pages/addPlaylist.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Add Playlist");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
