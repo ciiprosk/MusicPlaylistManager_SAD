b3a163f0790f48ecce1c226106aacf0761ee9847
OmarSilano <silanoomar@gmail.com>
2026-06-03 17:32:28 +0200

    creazione TrackObserver

--- src/main/java/it/diem/unisa/musicmanager/MusicPlaylistManager.java
+++ src/main/java/it/diem/unisa/musicmanager/MusicPlaylistManager.java
@@ -30,7 +30,7 @@ public class MusicPlaylistManager extends Application {
         PersistenceService persistenceService = new PersistenceService(trackDAO, playlistDAO, sharedState);
         persistenceService.load();
         PlaylistService playlistService = new PlaylistService(playlistDAO, sharedState);
-        TrackService trackService = new TrackService(trackDAO, playlistDAO, sharedState);
+        TrackService trackService = new TrackService(trackDAO, sharedState);
 //        PlayerService playerService = new PlayerService(sharedState);
 
         FXMLLoader fxmlLoader = new FXMLLoader(MusicPlaylistManager.class.getResource("MusicPlaylistManagerGUI.fxml"));
