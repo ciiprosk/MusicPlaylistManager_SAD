906f3e550e611b645d8ea2d2acb09de290620233
Daniele Robinia <g.robinia@studenti.unia.it>
2026-06-03 20:18:16 +0200

    Aggiunta ulteriori test sulle playlist, JSONPlaylistDAO e 2 metodi in PlaylistService

--- src/test/java/it/diem/unisa/musicmanager/dao/JSONPlaylistDAOTest.java
+++ src/test/java/it/diem/unisa/musicmanager/dao/JSONPlaylistDAOTest.java
@@ -5,9 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.*;
 
 class JSONPlaylistDAOTest {
 
@@ -78,5 +77,52 @@ class JSONPlaylistDAOTest {
         assertEquals("Pop", result.get().getName());
         assertEquals(1, result.get().getTracks().size());
         assertTrue(result.get().containsTrack(trackId));
+    }
+
+    //TEST RICERCA PLAYLIST PER ID FUNZIONANTE
+
+    @Test
+    void searchByIdShouldReturnSpecificPlaylist() {
+
+        Playlist playlist = new Playlist("Rock");
+
+        playlistDAO.insert(playlist);
+
+        Optional<Playlist> result =
+                playlistDAO.searchById(playlist.getId());
+
+        assertTrue(result.isPresent());
+        assertEquals("Rock", result.get().getName());
+    }
+
+    //TEST RIMOZIONE DI UNA TRACCIA DA UNA PLAYLIST AGGIORNA IL FILE
+
+    @Test
+    void updateShouldSavePlaylistAfterRemovingTrackOnFile() {
+
+        Playlist playlist = new Playlist("Rock");
+
+        UUID trackId = UUID.randomUUID();
+
+        playlist.addTrack(trackId);
+
+        playlistDAO.insert(playlist);
+
+        playlist.removeTrack(trackId);
+
+        playlistDAO.update(playlist);
+
+        DAO<Playlist> newPlaylistDAO =
+                new JSONPlaylistDAO(
+                        "test-data",
+                        "playlists-test.jsonl"
+                );
+
+        Optional<Playlist> result =
+                newPlaylistDAO.searchById(playlist.getId());
+
+        assertTrue(result.isPresent());
+        assertFalse(result.get().containsTrack(trackId));
+        assertTrue(result.get().getTracks().isEmpty());
     }
 }
