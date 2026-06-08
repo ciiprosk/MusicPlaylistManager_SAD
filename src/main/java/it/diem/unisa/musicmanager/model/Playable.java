846348602eeb3087d2ad9904a101d8098b0a1e37
ciiprosk <rosapassaro28@gmail.com>
2026-06-08 17:41:45 +0200

    create classi per cmposite

--- src/main/java/it/diem/unisa/musicmanager/model/Playable.java
+++ src/main/java/it/diem/unisa/musicmanager/model/Playable.java
@@ -1,4 +1,7 @@
 package it.diem.unisa.musicmanager.model;
 
+import java.util.List;
+
 public interface Playable {
+    List<Track> getTracksToPlay();
 }
--- src/main/java/it/diem/unisa/musicmanager/model/QueueItem.java
+++ src/main/java/it/diem/unisa/musicmanager/model/QueueItem.java
@@ -1,4 +1,8 @@
 package it.diem.unisa.musicmanager.model;
 
+/**
+ * La classe QueueItem rappresenta un elemento della coda di riproduzione.
+ * Ogni traccia può essere o associata a una playlist oppure
+ */
 public class QueueItem {
 }
